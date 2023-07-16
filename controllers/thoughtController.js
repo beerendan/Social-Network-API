4bfb05acda379ad99b8eff353687c08001d18836
Beerendan <brendanrbowen@gmail.com>
2023-07-16 16:37:58 -0400

    added create and update thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -17,9 +17,47 @@ const thoughtControl={
             res.status(400).json(err)});
     },
     //create a thought
-
+    createThought({params,body}, res){
+        Thought.create(body)
+        .then(({_id})=>{
+            return User.findOneAndUpdate({
+                username:body.username
+            },
+            {
+                $push:{thoughts:_id}
+            },
+            {
+                new:true
+            });
+        })
+        .then(userData=>{
+            if (!userData){
+                res.status(404).json({
+                    message:`No user with id:${body.username}`
+                });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err=> res.json(err));
+    },
     //update a thought
-
+    updateThought({params,body},res){
+        Thought.findOneAndUpdate({_id:params.id},
+            body,{
+                new:true,
+                runValidators:true
+            })
+            .then(updatedThought=>{
+                if(!updatedThought){
+                    return res.status(404).json({
+                        message: `No thought found with id: ${params.id}`
+                    })
+                }
+                res.json(updatedThought);
+            })
+            .catch(err=>res.json(err));
+    },
     //delete a thought
 
     //create a reaction
