779c39c05427136b9719286bb2c4c4e82ea93738
Beerendan <brendanrbowen@gmail.com>
2023-07-16 17:42:26 -0400

    completed thought route

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -1,1 +1,25 @@
 const router=require('express').Router();
+
+const{
+    getAllThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+}=require('../../controllers/thoughtController');
+
+//all thoughts and new thought route
+router.route('/').get(getAllThoughts).post(createThought);
+
+//individual thought route for updating and deleting routes
+router.route('/:id').get(getOneThought).put(updateThought).delete(deleteThought);
+
+//route for adding reaction
+router.route('/:thoughtId/reactions').post(createReaction);
+
+//delete reaction route
+router.route('/thoughtId/reactionId').delete(deleteReaction);
+
+module.exports=router;
