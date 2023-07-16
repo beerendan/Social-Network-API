a3ef88c784fe0ddf2e100110931fb1e552dc5848
Beerendan <brendanrbowen@gmail.com>
2023-07-16 15:05:18 -0400

    indexes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const mongo=require('mongoose');
 
 //app setup
-const app=express()
+const app=express();
 const PORT=process.env.PORT||3001;
 
 app.use(express.urlencoded({extended:true}));
